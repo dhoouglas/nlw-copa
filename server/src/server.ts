418de3c916feb5548347a2e1bdb9cbfea8f900dc
unknown <dhoouglas@hotmail.com>
2022-10-31 22:18:38 -0300

    Config cors and prisma generator

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,18 @@
 import Fastify from 'fastify';
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 const prima = new PrismaClient({
     log: ['query'],
 });
 
-
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
+    });
+
+    await fastify.register(cors, {
+        origin: true,
     });
 
     // http://localhost:3333/pools/count
@@ -18,7 +22,7 @@ async function bootstrap() {
         return  { count }
     });
 
-    await fastify.listen({ port: 3333 });
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
