504f55dd94e9baf8c78b6cd531059e1c55aebbed
unknown <dhoouglas@hotmail.com>
2022-10-31 21:46:16 -0300

    Install and config Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
         logger: true,
     });
 
+    // http://localhost:3333/pools/count
+    fastify.get('/pools/count', () => {
+        return  { count: 0 }
+    });
+
     await fastify.listen({ port: 3333 });
 }
 
