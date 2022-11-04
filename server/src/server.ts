bff2d196dfa3a648f97953331e565e7933d565cc
unknown <dhoouglas@hotmail.com>
2022-11-04 00:01:27 -0300

    Created router post pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
+import ShortUniqueId from 'short-unique-id';
 
-const prima = new PrismaClient({
+const prisma = new PrismaClient({
     log: ['query'],
 });
 
@@ -15,12 +17,35 @@ async function bootstrap() {
         origin: true,
     });
 
-    // http://localhost:3333/pools/count
+    // GET
     fastify.get('/pools/count', async () => {
-        const count = await prima.pool.count()
+        const count = await prisma.pool.count()
         
         return  { count }
     });
+
+    // POST
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toLocaleUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })
+
+        return reply.status(201).send({ code })        
+    })
+
+    
 
     await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
 }
