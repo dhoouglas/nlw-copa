9a84d487069e0c17be9c2d26e3ef26a9b28fc8fd
unknown <dhoouglas@hotmail.com>
2022-11-22 21:26:52 -0300

    Created Interface Find.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,6 +9,7 @@ import { Loading } from './src/components/Loading';
 
 import {THEME} from './src/styles/theme'
 import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -22,7 +23,7 @@ export default function App() {
             translucent
           />
           
-          { fontsLoaded ? <New /> : <Loading /> }
+          { fontsLoaded ? <Find /> : <Loading /> }
         </AuthContextProvider>
     </NativeBaseProvider>
     
